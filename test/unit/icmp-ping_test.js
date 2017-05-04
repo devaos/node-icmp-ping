d3421d2da7303b4da46f08fe2312081bab6b6385
Ari Aosved <ari.aosved@gmail.com>
2017-05-03 18:48:53 -1000

    Removing index file and now linting tests

--- test/unit/icmp-ping_test.js
+++ test/unit/icmp-ping_test.js
@@ -1,10 +1,22 @@
-let ping = require('../../lib/icmp-ping')
+/**
+ * Copyright (c) 2017 Ari Aosved
+ * http://github.com/devaos/node-icmp-ping/blob/master/LICENSE
+ */
 
-var assert = require('assert');
-describe('ping', function() {
-  describe('#probe()', function() {
-    it('should return an error with bad parameters', function(done) {
-      ping.probe(null, function(err) {
+'use strict';
+
+// =============================================================================
+
+const ping = require('../../lib/icmp-ping');
+
+// =============================================================================
+
+/* eslint-env and, mocha */
+
+describe('ping', () => {
+  describe('#probe()', () => {
+    it('should return an error with bad parameters', (done) => {
+      ping.probe(null, (err) => {
         if (err) done();
         else done(new Error('No error'));
       });
